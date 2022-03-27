c46eeaee7be650eb72f4857c97342bd5c281f25b
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2022-03-27 17:05:29 +0500

    first commit

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+// export const shuffleArray = (array: any[]) => {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// };
+
+export const shuffleArray = (array: any[]) =>
+  [...array].sort(() => Math.random() - 0.5);
